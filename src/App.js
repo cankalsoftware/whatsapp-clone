0a45fca1082d8822f24f6d52beac6189baa5f847
Agon Misini <am44519@ubt-uni.net>
2021-02-10 08:53:21 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Chat from './components/Chat';
 import Sidebar from './components/Sidebar';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        {/* Chat */}
+        <Chat />
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,19 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_KEY,
+    apiKey: "AIzaSyDKXB_DVyQwzPwBYpB-3O_QHAmOrwTfG1E",
     authDomain: "whatsapp-clone-65846.firebaseapp.com",
     projectId: "whatsapp-clone-65846",
     storageBucket: "whatsapp-clone-65846.appspot.com",
     messagingSenderId: "3103103476",
     appId: "1:3103103476:web:0741dfd55563348428c1a1",
     measurementId: "G-PNLE0TMERH"
-  };
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
